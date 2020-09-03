e1595fe2ea8b9ab1a58cc6e52ce445af5596dd70
Jazmin Matos <jmatos1993@gmail.com>
2020-09-03 03:03:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,18 +1,13 @@
 class School
   #attr_accessor :roster
-  attr_reader :name, :student_name, :grade
+  attr_reader :name, :student_name, :grade, :roster
   
   #part 1
   def initialize(name)  
     @name = name
-        @roster = {}
-
-  end
-  
-  #part 2
-  def roster #reader/getter method 
     @roster = {}
   end
+
   
   #part 3
   def add_student(student_name, grade)
