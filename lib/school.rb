cdd6a9b94f82ecb77c7ff326dde9fec576700958
Jazmin Matos <jmatos1993@gmail.com>
2020-09-03 03:07:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,7 +12,7 @@ class School
   #part 3
   def add_student(student_name, grade)
     if #grade exists
-    
+      
     else  
       @roster[grade] = [] #establish that this key will have an array value
       @roster[grade] << student_name #put items in this array value for this key
