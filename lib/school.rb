12d7286fe13d25f1d27955c24a08b354663d072d
Jazmin Matos <jmatos1993@gmail.com>
2020-09-03 02:53:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  #attr_accessor
+  attr_reader :name, :student_name, :grade
+  
+  #part 1
+  def initialize(name)  
+    @name = name
+  end
+  
+  #part 2
+  def roster #reader/getter method 
+    @roster = {}
+  end
+  
+  #part 3
+  def add_student(student_name, grade)
+    @roster[grade] = []
+    @roster[grade] << student_name
+  end
+
+end
+
+
+#roster = hash: key -> grade, value -> array of student names 
